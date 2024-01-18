5c940b8f0ba5b96e96481ce885ad81d19b1882ca
RonexCM <98376365+RonexCM@users.noreply.github.com>
2024-01-18 18:45:14 +0545

    Removed unwanted files and worked on the feedabcak for rtk query and loader

--- src/pages/student/Register.tsx
+++
@@ -1,5 +0,0 @@
-const Register = () => {
-  return <div>Register Form</div>;
-};
-
-export default Register;
--- src/validation/userProfileValidationSchema.tsx
+++ src/validation/userProfileValidationSchema.tsx
@@ -3,10 +3,6 @@ import * as Yup from "yup";
 const userProfileValidationSchema = Yup.object().shape({
   name: Yup.string().required("Name is required"),
   description: Yup.string().required("Description is required"),
-  mail: Yup.string()
-    .email("Invalid email address")
-    .required("Email is required"),
-  phone: Yup.string().required("Phone number is required"),
 });
 
 export default userProfileValidationSchema;
