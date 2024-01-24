e03a6c6d1d2a9be41abc50bdb5cbfff610df13cd
RonexCM <98376365+RonexCM@users.noreply.github.com>
2024-01-24 10:52:59 +0545

    made few changes to user navbar and removed unnecessary comments and files

--- src/layouts/UserNavbar.tsx
+++ src/layouts/UserNavbar.tsx
@@ -1,6 +1,5 @@
 import { NavLink } from "react-router-dom";
 import skillshoreLogo from "../assets/skillshoresvg.svg";
-import { FaHouse } from "react-icons/fa6";
 import Cookies from "universal-cookie";
 import { logOut } from "../redux/slice/userSlice";
 import { useDispatch } from "react-redux";
@@ -14,8 +13,7 @@ const UserNavbar = () => {
   return (
     <nav className="h-[56px] shrink-0 w-full bg-[#03103F] text-white flex justify-between items-center px-12">
       <div className="flex items-center gap-3 cursor-pointer">
-        <FaHouse className="ml-5 text-gray-200" />
-        <NavLink to="/profile" className="login-nav flex gap-2">
+        <NavLink to="." className="login-nav flex gap-2">
           <img src={skillshoreLogo} alt="skillshore-logo" />
         </NavLink>
       </div>
@@ -35,23 +33,6 @@ const UserNavbar = () => {
           Logout
         </NavLink>
       </div>
-      {/* <ul className="font-medium px-10 py-4 flex space-x-5 cursor-pointer justify-end">
-        <li>
-          <NavLink
-            to="/profile"
-            className="cursor-pointer mr-5 dark:text-white font-bold"
-          >
-            Profile
-          </NavLink>
-          <NavLink
-            to="/"
-            className="cursor-pointer font-normal dark:text-white"
-            onClick={LogOut}
-          >
-            Logout
-          </NavLink>
-        </li>
-      </ul> */}
     </nav>
   );
 };
