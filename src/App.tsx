70b969fbd295af00850bd6c0d923e05e751e5420
RonexCM <98376365+RonexCM@users.noreply.github.com>
2024-01-24 12:54:25 +0545

    Added protected route to userprofile and EditProfile

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { EnterNewPassword, ForgotPassword } from "./pages/auth/forgotPassword";
+import {  ProtectedRoute } from "./layouts";
 import { UserProfile, EditProfile } from "./pages/student/profile";
 import { Home } from "./pages/student/home";
 import { Login } from "./pages/auth/login";
@@ -20,8 +21,14 @@ const App = () => {
           {/* ----------user pages goes here as protected route---------- */}
           <Route element={<CommonLayout layoutFor="user" />}>
             <Route path="/home" element={<Home />} />
-            <Route path="/profile" element={<UserProfile />} />
-            <Route path="/editProfile" element={<EditProfile />} />
+            <Route
+              path="/profile"
+              element={<ProtectedRoute Component={UserProfile} />}
+            />
+            <Route
+              path="/editProfile"
+              element={<ProtectedRoute Component={EditProfile} />}
+            />
           </Route>
           {/* ----------admin pages goes here as protected route----------- */}
         </Routes>
