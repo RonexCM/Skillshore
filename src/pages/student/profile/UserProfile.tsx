ce8c34b94dd1e20dff2e46b88546b07b70834c18
Udaya <udaya.panday@gmail.com>
2024-01-26 12:19:02 +0545

    updated folder structure, fixed some design inconsistency

--- src/pages/student/profile/UserProfile.tsx
+++ src/pages/student/profile/UserProfile.tsx
@@ -54,6 +54,21 @@ const UserProfile = () => {
                   <ProfileDetails
                     title={"Skills"}
                     data={userDetails?.profile?.skills?.map((skill, index) => (
+=======
+                    data={userDetails.profile.education}
+                  />
+                  <ProfileDetails
+                    title={"Career "}
+                    data={userDetails.profile.career}
+                  />
+                  <ProfileDetails
+                    title={"Experience"}
+                    data={userDetails.profile.experience}
+                  />
+                  <ProfileDetails
+                    title={"Skills"}
+                    data={userDetails.profile?.skills?.map((skill, index) => (
+>>>>>>> 926278e (updated folder structure, fixed some design inconsistency)
                       <li className="list-none" key={index}>{`${
                         index + 1
                       }. ${skill}`}</li>
