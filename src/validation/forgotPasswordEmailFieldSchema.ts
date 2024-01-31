e5876ec93074add1fd093a951c9fa1fea6bc09a2
Aashutosh Lekhak <lekhakaashutosh218@gmail.com>
2024-01-31 11:48:59 +0545

    fixed unresponsiveness in email field

--- src/validation/forgotPasswordEmailFieldSchema.ts
+++ src/validation/forgotPasswordEmailFieldSchema.ts
@@ -2,10 +2,10 @@ import * as Yup from "yup";
 
 const forgotPasswordEmailFieldSchema = Yup.object({
   email: Yup.string()
-    .email("Invalid email address")
+    .email("Email must be a valid email")
     .required("Email is required")
     .matches(
-      /^[a-zA-Z0-9]+([._-]*[a-zA-Z0-9]+)*@[a-zA-Z0-9]+([a-zA-Z0-9]*[a-zA-Z0-9]+)*\.[a-zA-Z]{2,}$/,
+      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i,
       "Email must be a valid email"
     ),
 });
